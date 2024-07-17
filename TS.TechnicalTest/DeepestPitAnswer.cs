013021468e5141a538e0dcd8d1788e1859bc1b26
AndileM001 <andilemdakane@yahoo.com>
2024-07-17 19:05:49 +0200

    Q and R assigned

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -8,9 +8,19 @@ public class DeepestPitAnswer
         for (int i = 0; i < points.Length; i++) {
             //check if A[P] > A[P+1]
             var P = i;
+            Nullable<int> Q = null;
+            Nullable<int> R = null;
             if (points[P] > points[P + 1])
             {
-
+                Q = P + 1;
+            }
+            //check if Q is assigned 
+            if (Q != null) {
+                // check if A[Q] < A[Q+1]
+                if (points[Q.Value] < points[Q.Value + 1])
+                {
+                    R = Q.Value + 1;
+                }
             }
 
         }
