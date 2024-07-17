aa4d7fbef9f35e31e605ee14bd56856f2b6d2a36
AndileM001 <andilemdakane@yahoo.com>
2024-07-17 18:23:17 +0200

    commit

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,6 +5,16 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+        int totalWords = 0 ;
+        var sentences = s.Split(new char[] {'.', '?' , '!' });
+
+        foreach (var sentence in sentences) {
+            //split words in the sentence by space
+            var words = sentence.Split(' ');
+            int wordsLength = words.Length;
+
+        }
+
+        return totalWords ;
     }
 }
